7fe6a54433f546ae7c12fdae68574f98398680d8
Orion Lawlor <lawlor@alaska.edu>
2025-11-03 11:36:40 -0900

    Stack display is now "texture_from_stack", in-memory framebuffer is texture_from_memory.

--- SDL2_texture_from_stack/main.cpp
+++ SDL2_texture_from_stack/main.cpp
@@ -1,7 +1,8 @@
 /* 
 Simple Directmedia Library (SDL2) Texture from Memory Example
 
-This example draws a block of memory using SDL2.
+This example draws the runtime stack (!) using SDL2.
+Calling functions changes the stack binary data, which looks neat.
 
 SDL example originally by Andrew Baluyot (2024-11-15)
 Sprites added by Orion Lawlor, lawlor@alaska.edu, 2025-10-09 (Public Domain)
@@ -24,27 +25,47 @@ void fail(const char *why) {
     exit(1);
 }
 
+// Event handling, see https://www.libsdl.org/release/SDL-1.2.15/docs/html/sdlevent.html
 
-// This is the block of memory we display onscreen
-const int mem_w = 256;
-const int mem_h = 256;
-int mem_framebuffer[mem_w*mem_h]; // framebuffer data storage
+void *compiler_deoptimizer=0; // <- keeps compiler from optimizing stuff away
 
-// Example of how to draw things to the in-memory framebuffer
-void write_pixels() {
-	for (int y=0;y<100;y++)
-	for (int x=0;x<100;x++)
-	{
-		int i = x + y*mem_w; // index for pixel (x,y)
-		mem_framebuffer[i] = 0xff0000; // red as hex (0xRRGGBB)
-	}
+// Write a bunch of stack space
+void doStackW()
+{
+    const int n=50000;
+    char buf[n];
+    for (int i=0;i<n;i++) buf[i]=0x80; // gray
+    compiler_deoptimizer = &buf;
+    printf("Uploaded some stack space at %p\n",compiler_deoptimizer);
 }
 
-// Event handling, see https://www.libsdl.org/release/SDL-1.2.15/docs/html/sdlevent.html
+// Recursive call
+
+// Fibonacci has low total stack depth
+int recfib(int i) {
+    if (i<2) return 1;
+    else return recfib(i-1) + recfib(i-2);
+}
+
+// Recursive decrement has high total stack depth
+int recdec(int i) {
+    if (i<2) return 1;
+    else return recdec(i-1) + 1;
+}
+
+void doStackR() {
+    //recfib(10);
+    recdec(1000);
+}
+
+
 void doKey(SDL_KeyboardEvent *event, int down) {
     // Handle keyboard event here!
-    if (down && event->keysym.sym==SDLK_w) { // write some pixels onscreen
-	write_pixels();
+    if (down && event->keysym.sym==SDLK_w) { // write a bunch of stack space
+        doStackW();
+    }
+    if (down && event->keysym.sym==SDLK_r) { // recurse through a bunch of stack space
+        doStackR();
     }
     if (down && event->keysym.sym==SDLK_SPACE) { // spacebar quits
         exit(0);
@@ -95,26 +116,28 @@ void drawTexture(SDL_Texture *texture,int x,int y, int w,int h)
 
 
 int main() {
-    // Zoom factor during render
-    int zoom=3;
-    
     if (SDL_Init(SDL_INIT_VIDEO) < 0) fail("init SDL");
     IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG); // load PNG or JPEG images
     
-    window = SDL_CreateWindow("Memory Display", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, mem_w*zoom, mem_h*zoom, 0);
+    window = SDL_CreateWindow("Stack Memory Display", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 512, 0);
     if (!window) fail("Create window");
     
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) fail("Create renderer");
     
     
+    // Size of memory area to display
+    int w=256, h=128;
+    
+    // Zoom factor during render
+    int zoom=4;
     
     // See: https://wiki.libsdl.org/SDL2/SDL_CreateTexture
     // See: https://wiki.libsdl.org/SDL2/SDL_PixelFormatEnum
     SDL_Texture *tex = SDL_CreateTexture(renderer,
-        SDL_PIXELFORMAT_ARGB8888,
+        SDL_PIXELFORMAT_RGB888,
         SDL_TEXTUREACCESS_STREAMING,
-        mem_w,mem_h);
+        w,h);
     if (!tex) fail("CreateTexture");
     
     while (1) { // main loop of game
@@ -124,12 +147,17 @@ int main() {
         
         SDL_Rect rect;
         rect.x=0; rect.y=0;
-        rect.w=mem_w; rect.h=mem_h;
+        rect.w=w; rect.h=h;
+        int pitch=w*3;
+        const void *pixels=
+            //(const void *)&SDL_CreateTexture; // library's machine code
+            (((const char *)&rect)-3*w*h); // the stack (live!)
         
         // See https://wiki.libsdl.org/SDL2/SDL_UpdateTexture
-        if (SDL_UpdateTexture(tex,&rect,mem_framebuffer,4*mem_w)) fail("UpdateTexture");
+        if (SDL_UpdateTexture(tex,&rect,pixels,pitch)) fail("UpdateTexture");
         
-        drawTexture(tex,0,0,mem_w*zoom,mem_h*zoom);
+        drawTexture(tex,0,0,w*zoom,h*zoom);
+        
         
         SDL_RenderPresent(renderer); // show rendered stuff
         
